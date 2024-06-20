fa1fa2b70e4a1afc20e824742ac03c69295c3d16
Felipe Pontes <pontesfelipe835@gmail.com>
2024-06-20 10:59:21 -0300

    feat: add button, grid new

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,45 +1,56 @@
-import { Grid, styled } from "@mui/material"
+import { Grid, styled, Container, Typography, Button } from "@mui/material"
 import Avatar from "../../../../images/avatar.jpg"
+import DownloadIcon from '@mui/icons-material/Download';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+
 
 
 const Hero = () => {
   
     const StyledHero = styled("div") (()=> ({
       
-      backgroundColor: "black"
+      backgroundColor: "black",
+      height: "100vh"
       
     })) 
 
     const StyledImg = styled("img") (()=> ({
       
-      width: "30%",
+      width: "100%",
       borderRadius: "50%"
       
     })) 
 
     return (
       <>
-        <StyledHero>
-
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-              
+          <StyledHero>
+            <Container>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={4}>
+                  <StyledImg src={Avatar} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                  <Typography color="primary" variant="h1" textAlign="center">Felipe Pontes</Typography>
+                  <Typography color="primary" variant="h2" textAlign="center">Student Software Engineering</Typography>
+                  <Grid container>
+                    <Grid item xs={12} md={6}>
+                      <Button>
+                        <DownloadIcon/>
+                        Download CV
+                      </Button>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <Button>
+                        <AlternateEmailIcon/>
+                        Contact me
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-              
-            </Grid>
-            <Grid item xs={4}>
-              
-            </Grid>
-            <Grid item xs={8}>
-              
-            </Grid>
-          </Grid>
-          Ola
-          <StyledImg src={Avatar} />
-
-        </StyledHero>
-      </>
+            </Container>
+          </StyledHero>
+        </>
     )
   }
   
